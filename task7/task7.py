220d1d685947ac65a1e5fe5e594cc8fed127ae06
harikishore <harikishore4461@gmail.com>
2020-11-29 20:05:09 +0530

    task7 done

---
+++ task7/task7.py
@@ -0,0 +1,21 @@
+#import user defined module
+import mod
+print(mod.l1)
+mod.l1=[i-1 for i in mod.l1]
+print(mod.l1)
+
+#pandas package
+import pandas as p
+d=['h','e','l','l','o']
+s=p.Series(d)
+print(s)
+
+#random module
+import random as r
+x=list(range(1,101))
+print(r.choice(x))
+
+#sys package & python path
+import sys
+for p in sys.path:
+    print(p)
